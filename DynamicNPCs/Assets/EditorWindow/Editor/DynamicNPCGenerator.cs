fd7864bc731b47797beb981374c5445a44e109a9
iota547 <juliand547@utexas.edu>
2020-11-21 14:41:30 -0600

    Basic Editor Window Created
    basic editor window has been created for generating NPC's for later on when we get all the variables and conditionals setup

---
+++ DynamicNPCs/Assets/EditorWindow/Editor/DynamicNPCGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DynamicNPCGenerator : EditorWindow
+{
+    //Variables that will be displayed for developer to edit
+    string npcBaseName = "";
+    int npcID = 1;
+    SpriteRenderer npcSprite;
+    float npcScale;
+
+    //Other Variables
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [MenuItem("Tools/Dynamic NPC Generator")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(DynamicNPCGenerator));
+    }
+
+    //These will be the values actually displayed and changable (depending) for developer use
+    private void OnGUI()
+    {
+        GUILayout.Label("Generate New NPC", EditorStyles.boldLabel);
+
+        npcBaseName = EditorGUILayout.TextField("NPC Name", npcBaseName);
+        npcID = EditorGUILayout.IntField("NPC ID", npcID);
+        npcScale = EditorGUILayout.Slider("NPC Scale", npcScale, 0.5f, 3f);
+        npcSprite = EditorGUILayout.ObjectField("NPC Sprite", npcSprite, typeof(SpriteRenderer), false) as SpriteRenderer;
+
+        if (GUILayout.Button("Generate NPC"))
+        {
+            //generate NPC into scene
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
