74b98787a16d001c1a84283fc3e22c414bf5117b
iota547 <juliand547@utexas.edu>
2020-11-21 17:55:43 -0600

    Updates to Dialogue Management
    as stated, still figuring out dialouge manager stuff and how to have it take in strings of text

--- DynamicNPCs/Assets/Scripts/DialogueManager.cs
+++ DynamicNPCs/Assets/Scripts/DialogueManager.cs
@@ -12,4 +12,9 @@ public class DialogueManager : MonoBehaviour
         sentances = new Queue<string>();
     }
 
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting Converstation");
+    }
+
 }
