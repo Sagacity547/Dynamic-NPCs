421ac9626bdf97ebe36467a44b1a6d3737d62e84
iota547 <juliand547@utexas.edu>
2020-11-23 20:06:10 -0600

    Basic Dialogue System Complete
    basic dialouge system implemented, need to change code to activate based off of the player

--- DynamicNPCs/Assets/Scripts/DialogueManager.cs
+++ DynamicNPCs/Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
     private Queue<string> sentances;
 
     // Start is called before the first frame update
@@ -14,7 +20,45 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
+        animator.SetBool("isOpen", true);
         Debug.Log("Starting Converstation");
+        nameText.text = dialogue.name;
+        sentances.Clear();
+
+        foreach (string sentance in dialogue.sentances)
+        {
+            sentances.Enqueue(sentance);
+        }
+
+        DisplayNextSentance();
     }
 
+    public void DisplayNextSentance()
+    {
+        if (sentances.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentance = sentances.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentance(sentance));
+    }
+
+    IEnumerator TypeSentance (string sentance)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentance.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End of Conversation");
+    }
 }
