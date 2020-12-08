e85e3de96c468655664e5e78aa3ae81e4502f390
choneface <choneface@users.noreply.github.com>
2020-12-08 03:03:46 -0600

    Phonebook Update
    Wrote the start method to initally set the NPCs relationship statuses

---
+++ DynamicNPCs/Assets/Scripts/Phonebook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phonebook : MonoBehaviour
+{
+    // public static variables 
+    public static Dictionary<GameObject, int> phonebook = new Dictionary<GameObject, int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] NPCs = GameObject.FindGameObjectsWithTag("NPC");
+        foreach(GameObject NPC in NPCs) {
+           // Debug.Log("NPC : " + NPC.name + " Hash Code : " + NPC.GetHashCode());
+           // Debug.Log("Relationship : " + NPC.GetComponent<NPC>().currentRelationship);
+            phonebook.Add(NPC, NPC.GetComponent<NPC>().currentRelationship);
+        }
+        /*foreach(int i in phonebook.Values){
+            Debug.Log("Value: " + i);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
