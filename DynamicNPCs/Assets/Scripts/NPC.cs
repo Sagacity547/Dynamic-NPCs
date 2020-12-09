134eb80b796c83d4c271a30ec76ae5920f9f2308
iota547 <juliand547@utexas.edu>
2020-12-09 16:28:16 -0600

    Dialogue for different relationship levels
    added in dialogue options for different levels of NPC relationship with the player

--- DynamicNPCs/Assets/Scripts/NPC.cs
+++ DynamicNPCs/Assets/Scripts/NPC.cs
@@ -23,18 +23,25 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentRelationship == 0)
+        if(currentRelationship <= 50)
         {
+            sentancesSet = false;
             talkStranger();
-        } else if (currentRelationship <= 3)
+        } else if (currentRelationship <= 60)
         {
             //use aquantince dialogue
-        } else if (currentRelationship <= 6)
+            sentancesSet = false;
+            talkAquantince();
+        } else if (currentRelationship <= 80)
         {
             //use friend dialouge
-        } else if (currentRelationship <= 10)
+            sentancesSet = false;
+            talkFriend();
+        } else if (currentRelationship <= 100)
         {
             //use great friend dialogue
+            sentancesSet = false;
+            talkGreatFriend();
         }
     }
 
@@ -68,6 +75,56 @@ public class NPC : MonoBehaviour
     //Aquantince level Relationsip 1-3
     void talkAquantince()
     {
-        
+        if (!sentancesSet)
+        {
+            dialogue.sentances.Clear();
+            dialogue.sentances.Add("Hi there" + player.name);
+            dialogue.sentances.Add("What have you been up to recently?");
+
+            foreach (string sentance in talkQuest())
+            {
+                dialogue.sentances.Add(sentance);
+            }
+
+            dialogue.sentances.Add("Talk to you later then!");
+            sentancesSet = true;
+        }
+    }
+
+    void talkFriend()
+    {
+        if (!sentancesSet)
+        {
+            dialogue.sentances.Clear();
+            dialogue.sentances.Add("Hey" + player.name + "! How are you doing?");
+            dialogue.sentances.Add("Hope you've been doing well on your journeys");
+
+            foreach (string sentance in talkQuest())
+            {
+                dialogue.sentances.Add(sentance);
+            }
+
+            dialogue.sentances.Add("See you around, good luck out there!");
+            sentancesSet = true;
+        }
+    }
+
+    void talkGreatFriend()
+    {
+        if (!sentancesSet)
+        {
+            dialogue.sentances.Clear();
+            dialogue.sentances.Add(player.name + "! Nice to see you!");
+            dialogue.sentances.Add("You mind hanging out with me for a bit?");
+            dialogue.sentances.Add("I've just got a lot I want to talk about with you.");
+
+            foreach (string sentance in talkQuest())
+            {
+                dialogue.sentances.Add(sentance);
+            }
+
+            dialogue.sentances.Add("Alrighty then, see you around soon friend!");
+            sentancesSet = true;
+        }
     }
 }
